b8ee036be1c4412e43cdbae2ea0ae58c25bbe79b
toan.le <toan.le@63cebbd9-801a-4202-844a-6b85d0d5bae3>
2014-05-02 11:59:07 +0000

    update libs

--- trunk/App/Application/Resource/Cache.php
+++
@@ -1,9 +0,0 @@
-<?php
-class App_Application_Resource_Cache extends Zend_Application_Resource_ResourceAbstract
-{
-    public function init()
-    {
-        $options = $this->getOptions();
-        return $this;
-    }
-}
