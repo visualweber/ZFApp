f19e0ea9b50ad74faacbc454ce49e799edc9c3c9
hieuit7 <hieunguyenminh.93@gmail.com>
2015-09-08 17:44:54 +0700

    edit autoload classmaps

--- Application/Resource/Multidb.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * @desc  Override multidb resource
- * @author Toan LE <toan@visualweber.com>
- */
-class App_Application_Resource_Multidb extends Zend_Application_Resource_Multidb {
-}
--- Application/Resource/Router.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class App_Application_Resource_Router extends Zend_Application_Resource_Router {
-
-    public function init() {
-        parent::init();
-    }
-
-}
