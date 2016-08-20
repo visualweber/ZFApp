3580a507eec4a37b1b00f793797aad4dbed5ecce
Visual Weber Company Limited <visualweber@gmail.com>
2016-08-20 12:39:35 +0700

    Update Auth.php

--- src/App/Auth.php
+++ src/App/Auth.php
@@ -26,9 +26,9 @@
  * @copyright  Copyright (c) 2005-2010 Zend Technologies USA Inc. (http://www.zend.com)
  * @license    http://framework.zend.com/license/new-bsd     New BSD License
  */
-class App_Auth extends Zend_Auth {
+class Backend_Auth extends App_Auth {
 
-    protected $_client = 0;
+    protected $_client = 'viwebcms-backend';
     protected static $_session_id = '';
 
     /**
@@ -40,14 +40,17 @@ class App_Auth extends Zend_Auth {
      */
     public static function getInstance() {
         if (null === self::$_instance) {
-            // self::$_instance = new self();  
+            self::$_instance = new self();
         }
         self::$_instance = new self();
         try {
-            if (!Zend_Session::isStarted())
+            if (!Zend_Session::isStarted()):
                 Zend_Session::start();
+            endif;
         } catch (Zend_Session_Exception $e) {
-            echo "Cannot instantiate this namespace since \$lsession was created\n";
+            echo $e->getFile();
+            echo $e->getMessage();
+            echo $e->getTrace();
         }
         self::$_session_id = session_id();
         return self::$_instance;
@@ -57,40 +60,6 @@ class App_Auth extends Zend_Auth {
         if ($client) {
             $this->_client = $client;
         }
-    }
-
-    /**
-     * Returns true if and only if an identity is available from storage
-     *
-     * @return boolean
-     */
-    public function hasIdentity() {
-
-        return !$this->getStorage()->isEmpty($this->_client);
-    }
-
-    /**
-     * Returns the identity from storage or null if no identity is available
-     *
-     * @return mixed|null
-     */
-    public function getIdentity() {
-        $storage = $this->getStorage();
-
-        if ($storage->isEmpty($this->_client)) {
-            return null;
-        }
-
-        return $storage->read($this->_client);
-    }
-
-    /**
-     * Clears the identity from persistent storage
-     *
-     * @return void
-     */
-    public function clearIdentity() {
-        $this->getStorage()->clear($this->_client);
     }
 
 }
